a1a4c422dfcb020298b60c32cbed63967cc49799
alphajuliet <andrew@alphajuliet.com>
2021-08-22 16:30:28 +1000

    Update libraries to latest versions.

--- project.clj
+++ project.clj
@@ -3,10 +3,10 @@
   :url "https://alphajuliet.com/ns/jaipur-clj/"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.1"]
+  :dependencies [[org.clojure/clojure "1.10.3"]
                  [org.clojure/math.combinatorics "0.1.6"]
                  [random-seed "1.0.0"]
-                 [spec-dict "0.2.0"]
-                 [com.taoensso/tufte "2.1.0"]]
+                 [spec-dict "0.2.1"]
+                 [com.taoensso/tufte "2.2.0"]]
   :profiles {:dev {:plugins [[lein-topology "0.2.0"]]}}
   :repl-options {:init-ns jaipur-clj.core})
