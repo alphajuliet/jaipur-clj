02aa7dce542bc13d28740a66aab54c8e39cd8049
alphajuliet <andrew@alphajuliet.com>
2022-05-07 12:03:15 +1000

    Update Clojure

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://alphajuliet.com/ns/jaipur-clj/"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.3"]
+  :dependencies [[org.clojure/clojure "1.11.1"]
                  [org.clojure/math.combinatorics "0.1.6"]
                  [random-seed "1.0.0"]
                  [spec-dict "0.2.1"]
